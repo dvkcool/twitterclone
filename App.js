b96f11f71fd0968b536ae34d4eaf2bbd44732794
dvkcool <divyanshukumarg@gmail.com>
2017-12-10 00:13:03 +0530

    Completed Tabs using react-native + nested navigation instead of native-base

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { View, Text, Platform, StatusBar } from 'react-native';
+import React, {Component } from 'react';
+import { View, Text, Platform, StatusBar, TouchableHighlight } from 'react-native';
 import { TabNavigator } from 'react-navigation'; // 1.0.0-beta.14
-import {Icon} from 'native-base';
+import {Header, Left, Right, Content, Container, Body, Title, Thumbnail,
+        StyleProvider} from 'native-base';
+import {AppLoading } from 'expo';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
 import Ionicons from 'react-native-vector-icons/Ionicons'; // 4.4.2
 import Homescreen from './appres/Homescreen';
 import Searchscreen from './appres/Searchscreen';
@@ -66,8 +70,53 @@ const RootTabs = TabNavigator({
   },
   style: {
     backgroundColor: 'white',
-    paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+    paddingTop: 0,
   },
 }});
+class Basescreen extends Component{
+  state = {
+    fontLoaded: false,
+  };
 
-export default RootTabs;
+  async componentWillMount() {
+  await Expo.Font.loadAsync({
+    'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+  });
+  this.setState({ fontLoaded: true });
+}
+  render(){
+    if (!this.state.fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={this.componentWillMount}
+        onFinish={() => this.setState({ fontLoaded: true })}
+        onError={console.warn}
+      />
+    );
+  }
+  else{
+    return (
+      <StyleProvider style={getTheme(material)}>
+      <Container style={{paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,}}>
+      <Header hasTabs>
+      <Left>
+      <TouchableHighlight>
+      <Thumbnail small source={require("./appres/creativity.png")}/>
+      </TouchableHighlight>
+      </Left>
+      <Body>
+      <Title>Home</Title>
+      </Body>
+      <Right/>
+      </Header>
+      <RootTabs navigation={this.props.navigation}/>
+      </Container>
+      </StyleProvider>
+    );
+  }
+
+  }
+};
+
+export default Basescreen;
