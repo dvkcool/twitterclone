6939bb4a46b197636cdcd0376b0c432b240cb3e8
dvkcool <divyanshukumarg@gmail.com>
2017-12-10 14:13:13 +0530

    Drawer implemented succesfully

--- App.js
+++ App.js
@@ -3,46 +3,14 @@ import { View, Text } from 'react-native';
 import { DrawerNavigator } from 'react-navigation'; // 1.0.0-beta.14
 import Ionicons from 'react-native-vector-icons/Ionicons'; // 4.4.2
 import Mainscreen from './appres/Mainscreen';
-const HomeScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-  </View>
-);
-
-const ProfileScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Profile Screen</Text>
-  </View>
-);
-
-
-const RootDrawer = DrawerNavigator({
-  Home: {
-    screen: Mainscreen,
-    navigationOptions: {
-      drawerLabel: 'Home',
-      drawerIcon: ({ tintColor, focused }) => (
-        <Ionicons
-          name={focused ? 'ios-home' : 'ios-home-outline'}
-          size={26}
-          style={{ color: tintColor }}
-        />
-      ),
-    },
+import Sidebar from './appres/Sidebar';
+const HomeScreenRouter = DrawerNavigator(
+  {
+    Home: { screen: Mainscreen  },
   },
-  Profile: {
-    screen: ProfileScreen,
-    navigationOptions: {
-      drawerLabel: 'Profile',
-      drawerIcon: ({ tintColor, focused }) => (
-        <Ionicons
-          name={focused ? 'ios-person' : 'ios-person-outline'}
-          size={26}
-          style={{ color: tintColor }}
-        />
-      ),
-    },
-  },
-});
-
-export default RootDrawer;
+  {
+    // passing all route info as props to the custom Sidebar component
+    contentComponent: props => <Sidebar {...props} />
+  }
+);
+export default HomeScreenRouter;
---
+++ appres/Sidebar.js
@@ -0,0 +1,82 @@
+import React, {Component } from 'react';
+import {Container, Content, Header, Body, Left, Right,
+Card, CardItem, Icon, Thumbnail, H2, Footer, Text } from 'native-base';
+import {AppLoading } from 'expo';
+export default class Sidebar extends Component{
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+  render() {
+    if (!this.state.fontLoaded) {
+      return (
+      <AppLoading
+      startAsync={this.componentWillMount}
+      onFinish={() => this.setState({ fontLoaded: true })}
+      onError={console.warn}
+      />
+      );
+    }
+    else{
+      return (
+        <Container>
+            <Card>
+              <CardItem>
+                <Left>
+                  <Thumbnail source={require('./creativity.png')}/>
+                </Left>
+                <Body/>
+                <Right/>
+              </CardItem>
+              <CardItem>
+              <H2>Divyanshu Kumar</H2>
+              <Right>
+                <Icon name = 'ios-arrow-down-outline' style={{color:'#3F51B5'}}/>
+              </Right>
+              </CardItem>
+              <CardItem style={{paddingTop:0}}>
+                <Text style={{color: 'grey',}}>@DVk_cool</Text>
+              </CardItem>
+              <CardItem>
+                <Icon name='ios-person-outline'/>
+                <Text> Profile</Text>
+              </CardItem>
+              <CardItem>
+                <Icon name='ios-list-box-outline'/>
+                <Text> Lists</Text>
+              </CardItem>
+              <CardItem>
+                <Icon name='ios-flash-outline'/>
+                <Text> Moments</Text>
+              </CardItem>
+              <CardItem>
+                <Icon name='ios-browsers-outline'/>
+                <Text> Highlights</Text>
+              </CardItem>
+              <CardItem>
+                <Text> Setting And Privacy</Text>
+              </CardItem>
+              <CardItem>
+                <Text> Help Center</Text>
+              </CardItem>
+            </Card>
+          <Footer style={{  footerDefaultBg: '#fff'}} >
+            <Left>
+              <Icon style={{ marginLeft: 10 }} name='moon'/>
+            </Left>
+            <Right>
+              <Icon style={{ marginRight: 10 }} name='qr-scanner'/>
+            </Right>
+          </Footer>
+        </Container>
+      );
+    }
+  }
+}
